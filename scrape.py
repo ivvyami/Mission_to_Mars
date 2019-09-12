a59b90dadbd067864197cc681565ff9ef0d6ad26
Ivonne <ivvyami@outlook.com>
2019-09-12 13:11:43 -0500

    finished scrape.py

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
 from splinter import Browser
 from bs4 import Beautiful Soup
 import os
+import requests
+import pymongo
+import pandas as pd
 
 def init_browser(): 
     #path for chrome driver
@@ -41,13 +44,35 @@ def scrape():
     mars_online["featured_image_url"] = print(f"https://www.jpl.nasa.gov{img_link}")
 
 #MARS WEATHER
-    twitter_url = 'https://twitter.com/marswxreport?lang=en'
-    response = requests.get(twitter_url)
+    # twitter_url = 'https://twitter.com/marswxreport?lang=en'
+    # response = requests.get(twitter_url)
 
-    soup = BeautifulSoup(response.content, "html.parser")
+    # soup = BeautifulSoup(response.content, "html.parser")
+
+#MARS FACTS
+    facts_url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(facts_url)
+
+    #Get the table
+    mars_facts = tables[0]
+    mars_facts.columns = ['Info', 'Mars', 'Earth']
+    mars_facts.set_index('Info', inplace=True)
+    mars_facts.drop(columns= 'Earth')
+    mars_html_table = mars_facts.to_html()
+
+    mars_online["mars_html_table"] = mars_html_table
+
+#MARS HEMISPHERES
+    pic_url = 'https://astrology.usgs.gov'
+
+    hemisphere_image_urls = [
+    {"title": "Cerberus Hemisphere Enhanced", "img_url": f"{pic_url}/cache/images/cfa62af2557222a02478f1fcd781d445_cerberus_enhanced.tif_full.jpg"}, 
+    {"title": "Schiaparelli Hemisphere Enhanced", "img_url": f"{pic_url}/cache/images/3cdd1cbf5e0813bba925c9030d13b62e_schiaparelli_enhanced.tif_full.jpg"},
+    {"title": "Syrtis Major Hemisphere Enhanced", "img_url": f"{pic_url}/cache/images/ae209b4e408bb6c3e67b6af38168cf28_syrtis_major_enhanced.tif_full.jpg"},
+    {"title": "Valles Marineris Hemisphere Enhanced", "img_url": f"{pic_url}/cache/images/7cf2da4bf549ed01c17f206327be4db7_valles_marineris_enhanced.tif_full.jpg"}
+    ]
 
 
-
-    
+    mars_online["hemisphere_image_urls"] = hemisphere_image_urls
     
 
